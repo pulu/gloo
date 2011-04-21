eaf41596daef357015b732eccf8c055455d923cd
Raj <rajkumar.projects.db@gmail.com>
2011-04-21 12:53:35 -0400

    Pulu, first commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pulu::Application.config.session_store :cookie_store, :key => '_pulu_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pulu::Application.config.session_store :active_record_store
