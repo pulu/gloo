e20eeb1ea45760991b2cb7b3e673c4b18b733464
Raj <rajkumar.projects.db@gmail.com>
2011-04-23 11:31:40 -0400

    static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # return a title based on the page
+  def title
+    base_title = "PULU"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
