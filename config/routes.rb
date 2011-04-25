eb4138d321ec1fb65e6a42507792ba11fef97a84
Raj <rajkumar.projects.db@gmail.com>
2011-04-25 13:53:15 -0400

    Home Page layout Done

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Pulu::Application.routes.draw do
   get "pages/contact"
   get "pages/about"
 
-  # The priority is based upon order of creation:
+
+
+# The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
