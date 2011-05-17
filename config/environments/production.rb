4686d5608f1c8f88438f534640dedcd30ed1c70e
Raj <rajkumar.projects.db@gmail.com>
2011-05-17 00:28:38 -0400

    Typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Goo::Application.configure do
+Gloo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
